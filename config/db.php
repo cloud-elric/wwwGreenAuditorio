ed6b5c66e18c102c125295b8c044ff435d2f2c4f
cloud-elric <humberto@2gom.com.mx>
2017-05-15 12:02:24 -0500

    Inicio de proyecto

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mysql3000.mochahost.com;dbname=beto2gom_green_auditorio_telmex',
+    'username' => 'beto2gom_develop',
+    'password' => 'c0d1ngG33k',
+    'charset' => 'utf8',
+];
